da8c541ca88cf5c9f59092f73d82fde465c416bc
thelittlecitizen16 <thelittlecitizen16@gmail.com>
2020-08-11 17:15:00 +0300

    add IsCircular function to linkedlist project

---
+++ LinkedList/CirclularLinkedListValidator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedList
+{
+    public class CirclularLinkedListValidator 
+    {
+        public bool IsCircular<T>(LinkedList<T> linkedList)
+        {
+
+            if (linkedList.Count == 0)
+            {
+                return false;
+            }
+
+            LinkedListNode<T> node = linkedList.First;
+
+
+            if (linkedList.Count == 1)
+            {
+                if (node.Next == null)
+                {
+                    return false;
+                }
+            }
+
+            int count = 1;
+
+            while (count < linkedList.Count())
+            {
+                node = node.Next;
+                count++;
+            }
+
+            return !(node == linkedList.Last);        }
+    }
+}
